915e29cf50c8044bff302c0086d149fe32d2ea54
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-23 23:45:29 +0000

    fixed populate script for unit tests

--- populate_rango.py
+++ populate_rango.py
@@ -37,10 +37,9 @@ def populate():
             "Other Frameworks": {"views": 32, "likes": 16, "pages": other_pages}}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
