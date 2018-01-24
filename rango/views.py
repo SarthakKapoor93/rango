465994982ab8296ead219e2137d7af8e1bb4eb03
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-24 22:34:41 +0000

    updated views and added try except block for categories page

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -12,3 +12,19 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': "This tutorial has been put together by Sarthak Kapoor"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict ={}
+
+    try:
+       category = Category.objects.get(slug=category_name_slug)
+       pages = Page.objects.filter(category=category)
+       context_dict['pages'] = pages
+       context_dict['category'] = category
+
+    except Category.DoesNotExist:
+       context_dict['pages'] = None
+       context_dict['category'] = None
+
+
+return render(request, 'rango/category.html', context_dict)
