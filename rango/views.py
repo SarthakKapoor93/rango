4db68a14d6fb1aee265bdb171841026aafa7f65f
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-24 22:14:28 +0000

    modified index template to show categories on homepage

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
     context_dict = {'boldmessage': "This tutorial has been put together by Sarthak Kapoor"}
     return render(request, 'rango/about.html', context=context_dict)
-
-
