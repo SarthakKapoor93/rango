fc7085c93c06be137efcc01cef0a5c28d1a41c6a
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-24 22:46:48 +0000

    updated intex html to include slug category links

--- rango/views.py
+++ rango/views.py
@@ -13,18 +13,18 @@ def about(request):
     context_dict = {'boldmessage': "This tutorial has been put together by Sarthak Kapoor"}
     return render(request, 'rango/about.html', context=context_dict)
 
+
 def show_category(request, category_name_slug):
-    context_dict ={}
+    context_dict = {}
 
     try:
-       category = Category.objects.get(slug=category_name_slug)
-       pages = Page.objects.filter(category=category)
-       context_dict['pages'] = pages
-       context_dict['category'] = category
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
 
     except Category.DoesNotExist:
-       context_dict['pages'] = None
-       context_dict['category'] = None
+        context_dict['pages'] = None
+        context_dict['category'] = None
 
-
-return render(request, 'rango/category.html', context_dict)
+    return render(request, 'rango/category.html', context_dict)
