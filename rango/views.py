325db6c8894121345a9315fbc0df885ccf2b7f5e
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-24 23:21:08 +0000

    made changes for page name variable in index, category and views

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from rango.models import Category, Page
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages_list': pages_list}
+    context_dict = {'categories': category_list, 'pages': pages_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
