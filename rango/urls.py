833fc9a767c4d8ca8b55da2b28d4a1b89c2461e6
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-25 00:22:58 +0000

    added post request for category form

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^about', views.about, name='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request):
@@ -29,3 +30,19 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+    if form.is_valid():
+        cat = form.save(commit=True)
+        print(cat, cat.slug)
+        return index(request)
+    else:
+        print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
