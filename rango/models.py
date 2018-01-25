1cc50d6d487d0539c4ee9e0a0c05e6282f6603b3
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-01-25 15:41:50 +0000

    updated model and admin files with UserProfile function and class

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 
@@ -27,3 +28,12 @@ class Page(models.Model):
 
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
