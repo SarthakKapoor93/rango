4b375b35fff054f17db2fe8f31be48152de176dd
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-02-12 14:29:16 +0000

    fixed PageForm class URL for tests

--- rango/forms.py
+++ rango/forms.py
@@ -18,8 +18,8 @@ class CategoryForm(forms.ModelForm):
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
+    url = forms.CharField(max_length=200,
+                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
